1494373f1c8ae761607948dbf6238bb3757da73a
rubeneitor <56588038+rubeneitor@users.noreply.github.com>
2019-12-16 16:05:43 +0100

    CreacionProducto
    He creado el endoPoint para crear un producto

--- app/Controllers/ProductoController.js
+++ app/Controllers/ProductoController.js
@@ -9,7 +9,7 @@ export const getAllProductos = async (req, res) => {
 export const getProductoPorNombre = async (req, res) => {
     const nombre = req.params.nombre
 
-    Producto.findAll({ where: { nombre: nombre } }).then(producto => {
+    Producto.findAll({where: {nombre: nombre} }).then(producto => {
         res.send(producto)
     })
 
@@ -18,16 +18,26 @@ export const getProductoPorNombre = async (req, res) => {
 export const getProductosCarrito = async (req, res) => {
     const idCarrito = req.params.idCarrito
 
-    Producto.findAll({ where: { idCarrito: idCarrito } }).then(productoCarrito => {
+    Producto.findAll({where: {idCarrito: idCarrito}}).then(productoCarrito => {
         res.send(productoCarrito)
     })
 }
 
-//Operaciones Administrador
-
-export const añadirProducto = async (req, res) => {
-    const product = await Producto.create(req.body)
-    res.send(product)
-    
+//Añade un nuevo producto
+async function nuevoProducto(req, res, next) {
+	try {
+		const producto = await User.create(req.body);
+		res.status(200).json({
+			message: 'producto creado',
+            producto: producto,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			message: 'producto no creado',
+			error: error,
+		});
+	}
 }
+
 
