103851b76a2a4d41a9952d638600510a759c1476
rubeneitor <56588038+rubeneitor@users.noreply.github.com>
2019-12-20 12:53:14 +0100

    añadeCategory

--- models/Category.js
+++ models/Category.js
@@ -1,14 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
-    name: DataTypes.STRING
+    nombre: DataTypes.STRING, 
+    descripcion: DataTypes.STRING
   }, {});
   // Category.associate = function(models) {
 
   //   Category.belongsTo(models.Product);
 
   // };
-   Category.sync().catch(error =>
+   Category.sync({force:true}).catch(error =>
      console.error(`couldn't connect to database`, error),
    );
   return Category;
